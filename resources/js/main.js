a2bb6573921ff2daa73623cdd1e273fe6734a17d
Your Name <you@example.com>
2023-12-06 16:38:15 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+ //MIXITUP
+
+var mixer = mixitup('.work-grid');
+
+
+//MENU
+function openMenu() {
+    document.getElementById('menu-container').style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById('menu-container').style.width = "0";
+}
+
+
+//ANIMATION
+new WOW().init();
